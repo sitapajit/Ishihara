ca5151fa89546b16d2b06be4f54c827b957d04cd
sitapa <sitapajit@gmail.com>
2016-06-22 14:26:26 +0700

    get Answer Finish

--- Ishihara/ViewController.swift
+++ Ishihara/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var ishiharaImageView: UIImageView!
+    
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    //Explicit
+    var strAnswer:String = ""
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }   //Main Func
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }   //didReceive
 
+    
+    @IBAction func answerButton(sender: AnyObject) {
+        
+        strAnswer = String(answerTextField.text)
+        print("strAnswer ==> \(strAnswer)")
+        
+        
+    }   //AnswerButton
+    
+    
+    
 
-}
+}   //Main Class
 
